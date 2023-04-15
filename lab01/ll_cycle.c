8b458d3693c6e26c34589f23f29be6432693f409
Jekton Luo <ljtong64@gmail.com>
2023-04-15 16:31:39 +0800

    lab01 done

--- lab01/ll_cycle.c
+++ lab01/ll_cycle.c
@@ -2,6 +2,23 @@
 #include "ll_cycle.h"
 
 int ll_has_cycle(node *head) {
-    /* your code here */
+    if (head == NULL) return 0;
+    node *hare = head;
+    node *tortoise = head;
+
+    while (1) {
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        hare = hare->next;
+        if (hare == NULL) {
+            return 0;
+        }
+        tortoise = tortoise->next;
+        if (hare == tortoise) {
+            return 1;
+        }
+    }
     return 0;
 }
